b76ad1b9d73a7ac675b8c56d79f9d4b4f47b1feb
look0012 <46589934+look0012@users.noreply.github.com>
2020-02-20 09:31:49 +0800

    Add files via upload

--- easier-to-use version/iCDA-CGR (CircFunBase)/Predict.py
+++ easier-to-use version/iCDA-CGR (CircFunBase)/Predict.py
@@ -4,23 +4,26 @@ from numpy import *
 from sklearn.externals import joblib
 import  csv
 
+
+
 def ReadMyCsv(SaveList, fileName):
     csv_reader = csv.reader(open(fileName))
     for row in csv_reader:
         SaveList.append(row)
     return
+
 def StorFile(data, fileName):
     with open(fileName, "w", newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerows(data)
     return
+
 def list_toRow(x):
     y=[]
     for i in x:
 
         y.append(i[0])
     return y
-
 
 def Predict(circRNA,disease):
     List = []
@@ -41,7 +44,6 @@ def Predict(circRNA,disease):
 
     feature = []
 
-
     feature.extend(Sc[circRNA_List.index(circRNA)])
     feature.extend(Sd[Disease_List.index(disease)])
 
@@ -57,12 +59,8 @@ if __name__ == "__main__":
 
 
 
-
-
     circRNA="hsa_circ_0041477"
     disease="severe acne"
-
-
 
 
     circRNA, disease,score=Predict(circRNA,disease)
